c87a9291eecb1b61a3b0981dda2aa3c0403fc27e
Mohsen <MMM@Ayric.net>
2022-11-17 08:54:28 +0330

    Session_03

---
+++ 01.cs
@@ -0,0 +1,23 @@
+using System
+class test
+{
+public static void Main()
+{
+Console.WriteLine("enter number:”);
+string a = Console.ReadLine();
+int x = Convert.ToInt32(a);
+Console.WriteLine(tavan_2(x));
+Console.WriteLine(tavan_3(x));
+}
+private static double tavan_2 (int x)
+{
+double y = Math.Pow(x,2);
+double 7.= Math.Pow(x, 3);
+return (y);
+}
+private static double tavan_3(int x)
+{
+double y = Math.Pow(x,3);
+return
+}
+}
